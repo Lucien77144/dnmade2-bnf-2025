7287e0f1a3d0c18bbb31b25ea05f5947d29bd512
lucileguillo <lucile.guillo27@gmail.com>
2025-10-07 17:03:30 +0200

    feat: draggable element

--- scripts/views/book-restauration.js
+++ scripts/views/book-restauration.js
@@ -1,2 +1,18 @@
 const viewBookRestauration = document.querySelector('section#view-book-restauration');
 
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(Draggable)
+
+    const onDragStart = (e) => {
+            console.log(e)
+        }
+
+    const onDragEnd = (e) => {
+
+    }
+ 
+    Draggable.create(".draggable", {
+        inertia: true,
+        onDragStart,
+    });
+ });
