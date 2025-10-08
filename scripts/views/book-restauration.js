95fb612e1325fc4598c67646670173c1089db05d
Nolhan Pieroni <70480609+Non0reo@users.noreply.github.com>
2025-10-08 10:10:49 +0200

    fix: changed file path and added gem layout

--- scripts/views/book-restauration.js
+++ scripts/views/book-restauration.js
@@ -1,4 +1,29 @@
 const viewBookRestauration = document.querySelector('section#view-book-restauration');
+const gemsList = document.querySelectorAll('.gems-list');
+
+
+
+function pageInit() {
+
+  let count = 0;
+  gemsList.forEach((value, i) => {
+    for (let j = 0; j < 3; j++) {
+      //const element = array[i];
+      const gemContent = document.createElement('div');
+      gemContent.classList.add('gemContent')
+      gemContent.setAttribute('data-gemid', count);
+      value.appendChild(gemContent)
+
+      count++
+    }
+  })
+}
+
+pageInit();
+
+
+
+
 
 document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(Draggable)
