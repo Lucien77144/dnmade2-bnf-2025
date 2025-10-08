33b0dfca1b467daf09eb751a5e021c3da5e968bd
saraaaaaDM <desruelsarapro@gmail.com>
2025-10-08 14:17:19 +0200

    feat: html + organisation in brush.js

--- scripts/brush.js
+++ scripts/brush.js
@@ -1,24 +1,25 @@
+//a mettre dans le DOM
 document.addEventListener('DOMContentLoaded', () => {
-  fetch('../styles/views/brush.json')
+  fetch('/public/data/brush.json')
     .then((response) => response.json())
     .then((data) => {
       document.querySelector('#text').textContent = data.text;
     });
-    
-})
+  
+  //Partie de Sara
+  const brush = document.getElementById('brush');
+  const reveal = document.getElementById('revealImage');
+  let animating = false;
+  brush.addEventListener('click', () => {
+    if (animating) return;
+    animating = true;
+    brush.classList.add('active');
+    reveal.classList.add('active');
+  })
+  //Fin partie de Sara
+});
 
-const data = require ('../styles/views/brush.json');
+const data = require('/public/data/brush.json');
 console.log(data);
 
-const brush = document.getElementById('brush');
-const reveal = document.getElementById('revealImage');
-let animating = false;
 
-brush.addEventListener('click', () => {
-  if (animating) return;
-  animating = true;
-
-  brush.classList.add('active'); 
-  reveal.classList.add('active'); 
-
-});
