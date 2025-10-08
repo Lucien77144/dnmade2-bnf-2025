f53e2ffa31cdd9f4cbc436499df18854b3fe5479
LucienBS <79203857+Lucien77144@users.noreply.github.com>
2025-10-08 17:46:33 +0200

    fix zoom-book: js zoom inside;

--- scripts/views/zoom-book-end.js
+++ scripts/views/zoom-book-end.js
@@ -10,28 +10,52 @@ window.addEventListener('DOMContentLoaded', () => {
   -----------------------------------------------------
   -----------------------------------------------------*/
 
-  zoomInButton = document.getElementById('zoom-book-end-zoom-in-button')
-  zoomOutButton = document.getElementById('zoom-book-end-zoom-out-button')
-  zoomBookEndPhotoReliure = document.getElementById('zoom-book-end-image')
+  const zoomInButton = document.getElementById('zoom-book-end-zoom-in-button')
+  const zoomOutButton = document.getElementById('zoom-book-end-zoom-out-button')
+  const previewContainer = document.getElementById('zoom-book-end-container')
+  const previewMovingFrame = document.getElementById('zoom-book-end-image')
+
   let zoomIndex = 0
 
-  const SCALE_SIZES = [1, 1.2, 1.6, 2, 2.2]
-  function zoomIn() {
-    if (zoomIndex < SCALE_SIZES.length - 1) {
-      zoomIndex++
-    }
-    zoomBookEndPhotoReliure.style.transform = `scale(${SCALE_SIZES[zoomIndex]})`
-
-    // Add zoomed class when scale > 1
-    if (SCALE_SIZES[zoomIndex] > 1) {
-      bookZoomEndContainer.classList.add('zoomed')
-    }
+  const sizes = {
+    width: previewMovingFrame.offsetWidth,
+    height: previewMovingFrame.offsetHeight,
   }
-  const bookZoomEndContainer = document.getElementById(
-    'zoom-book-end-container'
-  )
 
-  const previewMovingFrame = document.querySelector('#zoom-book-end-image')
+  const SCALE_SIZES = [1.2, 1.6, 2, 2.2]
+  const tl = gsap.timeline({
+    duration: 0.5,
+    ease: 'ease-in-out',
+  })
+
+  function zoomIn() {
+    const scale = SCALE_SIZES[zoomIndex]
+    if (zoomIndex < SCALE_SIZES.length - 1) zoomIndex++
+
+    // Set the width scale
+    const currentWidth = sizes.width * scale
+    // previewMovingFrame.style.width = `${currentWidth}px`
+
+    // Instant finish previous zoom
+    tl.totalProgress(1)
+
+    // Set the width scale and scroll to center the image
+    tl.to(previewMovingFrame, {
+      width: `${currentWidth}px`,
+      onStart: () => {
+        previewContainer.classList.add('zoomed')
+      },
+      onUpdate: () => {
+        previewContainer.scrollTo({
+          left:
+            (previewContainer.scrollWidth - previewContainer.clientWidth) / 2,
+          top:
+            (previewContainer.scrollHeight - previewContainer.clientHeight) / 2,
+          behavior: 'instant',
+        })
+      },
+    })
+  }
 
   function bookZoomPreview(x, y) {
     const movingX = Math.floor((192 / x) * 100)
@@ -46,11 +70,9 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     previewMovingFrame.style.top = movingY + '%'
-
-    console.log(movingX, movingY)
   }
 
-  bookZoomEndContainer.addEventListener(
+  previewContainer.addEventListener(
     'touchmove',
     (event) => {
       const touchX = event.touches[0].offsetX
@@ -65,11 +87,11 @@ window.addEventListener('DOMContentLoaded', () => {
     if (zoomIndex > 0) {
       zoomIndex--
     }
-    zoomBookEndPhotoReliure.style.transform = `scale(${SCALE_SIZES[zoomIndex]})`
+    previewMovingFrame.style.transform = `scale(${SCALE_SIZES[zoomIndex]})`
 
     // Remove zoomed class when back to scale 1
     if (SCALE_SIZES[zoomIndex] <= 1) {
-      bookZoomEndContainer.classList.remove('zoomed')
+      previewContainer.classList.remove('zoomed')
     }
   }
 
