790d82eb39a42fb2e8959ef22278df03268ab5d0
Nolhan Pieroni <70480609+Non0reo@users.noreply.github.com>
2025-10-09 13:42:50 +0200

    refactor: Clean up code structure and remove commented-out sections

--- scripts/views/book-restauration.js
+++ scripts/views/book-restauration.js
@@ -1,19 +1,19 @@
+const viewBookRestauration = document.querySelector('section#view-book-restauration');
+const gemsList = document.querySelectorAll('.gems-list');
+const gameBook = document.querySelector('.game-book');
+
 let json;
+let popup = createPopup("#popup");
 
 document.addEventListener('DOMContentLoaded', async () => {
   gsap.registerPlugin(Draggable)
 
   const response = await fetch('../../public/data/book-restauration.json');
   json = await response.json();
-
   console.log(json);
+  
+  pageGameInit();
 });
-
-
-const viewBookRestauration = document.querySelector('section#view-book-restauration');
-const gemsList = document.querySelectorAll('.gems-list');
-const gameBook = document.querySelector('.game-book');
-
 
 function createPopup(id){
     let popupNode = document.querySelector(id);
@@ -33,10 +33,6 @@ function createPopup(id){
 
     return openPopup;
 }
-
-let popup = createPopup("#popup");
-
-
 
 
 
@@ -157,61 +153,4 @@ function pageGameInit() {
   });
 
 }
-
-pageGameInit();
-
-// function hitTest(item1, item2){
-//   console.log(item1, item2)
-// }
-
-/*document.addEventListener("DOMContentLoaded", (event) => {
-  gsap.registerPlugin(Draggable)
-
-  const overlapThreshold = "60%";
-  const item = ".gem-draggable"
-  const dropArea = ".drop__box";
-
-  // selection de la pierre
-
-// function dragDrop(e){
-
-// }
-
-  const onDragStart = (e) => {
-    gsap.to(item, {duration: 0.1, scale: 1.2, rotate: 'random(-9,9)', zIndex: 100})
-    console.log(e)
-  }
-
-  Draggable.create(item, {
-    inertia: true,
-    onDragStart,
-    onDragEnd: function(e) {
-  
-
-    if(!this.hitTest(dropArea, overlapThreshold)) {
-
-    gsap.to(item, {
-      duration: 0.7, 
-      x:0, 
-      y:0, 
-      scale: 1, 
-      rotate: 0, 
-      ease:'elastic.out(.45)'
-    });
-    } else {
-      gsap.to(item, {
-        x: 0,
-        y: 500,
-        scale: 0.5,
-        rotate: 0,
-        ease:'elastic.out(.45)',
-      })
-    }
-  
-  },
-  });
-});*/
-
-
-
 
