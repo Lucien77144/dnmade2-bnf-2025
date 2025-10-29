057059c86faf069673619fbe4fb1acc9dcc37c3f
VaLaBasEuhh <ratsimavaliavo@gmail.com>
2025-10-29 16:07:00 +0100

    Fix frise: fetch function for fonts deleted, text put in index.html

--- scripts/frise.js
+++ scripts/frise.js
@@ -5,26 +5,6 @@ let descriptions;
 let pointsBas;
 
 document.addEventListener('DOMContentLoaded', () => {
-  fetch('public/data/data.json')
-    .then((response) => response.json())
-    .then((data) => {
-      document.querySelector('#annee-1').textContent = data.annees[0].date
-      document.querySelector('#annee-2').textContent = data.annees[1].date
-      document.querySelector('#annee-3').textContent = data.annees[2].date
-      document.querySelector('#annee-4').textContent = data.annees[3].date
-      document.querySelector('#annee-5').textContent = data.annees[4].date
-
-      document.querySelector('#description-1').textContent =
-        data.annees[0].description
-      document.querySelector('#description-2').textContent =
-        data.annees[1].description
-      document.querySelector('#description-3').textContent =
-        data.annees[2].description
-      document.querySelector('#description-4').textContent =
-        data.annees[3].description
-      document.querySelector('#description-5').textContent =
-        data.annees[4].description
-    })
 
   points = document.getElementsByClassName('point')
   annees = document.getElementsByClassName('annee')
