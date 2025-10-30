0a8be1fbabee62b561327e5e76f1b27ad678fe94
New <naths.raf@gmail.com>
2025-10-30 16:16:55 +0100

    fix/forgot one path sorry girlz

--- scripts/views/step-2.js
+++ scripts/views/step-2.js
@@ -14,7 +14,7 @@ function start() {
   const feather = document.getElementById('feather')
   
   const sideImage = document.createElement('img')
-  sideImage.src = '../img/zoom.webp'
+  sideImage.src = '../public/images/zoom.webp'
   sideImage.alt = 'Deuxième manuscrit'
   sideImage.style.display = 'none'
   sideImage.classList.add('side-image')
