87a25b826c24bc0b769bcf0286655f50751a8da9
New <naths.raf@gmail.com>
2025-10-30 16:14:17 +0100

    fix images files relative paths

--- scripts/views/step-1.js
+++ scripts/views/step-1.js
@@ -1,4 +1,4 @@
-
+/* 
 let json
 fetch('../public/data/step-1.json')
 .then((Response) => Response.json())
@@ -7,14 +7,14 @@ json=step1
 })
 document.addEventListener('DOMContentLoaded', () => {
   start()
-})
+}) */
 
 function start() {
   const imgContainer = document.querySelector('.image')
   const mainImage = document.getElementById('mainImage')
 
   const sideImage = document.createElement('img')
-  sideImage.src = '../img/zoom.webp'
+  sideImage.src = '../public/images/zoom.webp'
   sideImage.alt = 'Deuxième manuscrit'
   sideImage.style.display = 'none'
   sideImage.classList.add('side-image')
